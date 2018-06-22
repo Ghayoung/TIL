370a3b8100e1ffed2db6b16248e9c431dacfd8c2
hayoung <wizardlover_@naver.com>
2018-06-22 19:59:36 +0900

    Style: main() 코드 변경

--- Blockchain/miniBlockChain/src/core/BlockChainStarter.java
+++ Blockchain/miniBlockChain/src/core/BlockChainStarter.java
@@ -3,13 +3,11 @@
 
 package core;
 
-import util.Util;
-
 public class BlockChainStarter {
 	
   public static void main(String[] args) {
 	  
-	  Block block = new Block(1, 0, "데이터");
+	  Block block = new Block(1, 0, "바뀐 데이터");
 	  block.mine();
 	  block.getInformation();
 
