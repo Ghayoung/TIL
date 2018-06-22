91a9ae63aa03d77250e443dddcd0f0bad166f3f6
hayoung <wizardlover_@naver.com>
2018-06-22 20:03:19 +0900

    feat: Add previousBlockHash

--- Blockchain/miniBlockChain/src/core/Block.java
+++ Blockchain/miniBlockChain/src/core/Block.java
@@ -7,7 +7,16 @@ public class Block {
 	private int blockID; //블록 번호
 	private int nonce; //정답 값
 	private String data; //데이터
+	private String previousBlockHash; //이전 블록의 해시 값
 	
+	public String getPreviousBlockHash() {
+		return previousBlockHash;
+	}
+
+	public void setPreviousBlockHash(String previousBlockHash) {
+		this.previousBlockHash = previousBlockHash;
+	}
+
 	public int getBlockID() {
 		return blockID;
 	}
@@ -32,16 +41,18 @@ public class Block {
 		this.data = data;
 	}
 	
-	public Block(int blockID, int nonce, String data) {
+	public Block(int blockID, int nonce, String data, String previousBlockHash) {
 		this.blockID = blockID;
 		this.nonce = nonce;
 		this.data = data;
+		this.previousBlockHash = previousBlockHash;
 	}
 	
 	//현재 특정한 블록이 가지는 정보 출력
 	public void getInformation() {
 		System.out.println("--------------------------------------");
 		System.out.println("블록 번호: " + getBlockID());
+		System.out.println("이전 해시: " + getPreviousBlockHash());
 		System.out.println("채굴 변수 값: " + getNonce());
 		System.out.println("블록 데이터: " + getData());
 		System.out.println("블록 해시: " + getBlockHash());
@@ -50,7 +61,7 @@ public class Block {
 	
 	
 	public String getBlockHash() {
-		return Util.getHash(nonce + data);
+		return Util.getHash(nonce + data + previousBlockHash);
 	}
 	
 	public void mine() {
