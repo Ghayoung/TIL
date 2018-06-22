19a5ec8f7e5841ca0dc5b1379ea9ff16e323fa53
hayoung <wizardlover_@naver.com>
2018-06-22 22:40:53 +0900

    feat: Add transactionList

--- Blockchain/Transaction/src/core/Block.java
+++ Blockchain/Transaction/src/core/Block.java
@@ -1,28 +1,30 @@
 package core;
 
+import java.util.ArrayList;
+
 import util.Util;
 
 public class Block {
 	
 	private int blockID; //블록 번호
-	private int nonce; //정답 값
-	private String data; //데이터
 	private String previousBlockHash; //이전 블록의 해시 값
+	private int nonce; //정답 값
+	private ArrayList<Transaction> transactionList; //트랜잭션 리스트
 	
-	public String getPreviousBlockHash() {
-		return previousBlockHash;
-	}
-
-	public void setPreviousBlockHash(String previousBlockHash) {
-		this.previousBlockHash = previousBlockHash;
-	}
-
 	public int getBlockID() {
 		return blockID;
 	}
 	
 	public void setBlockID(int blockID) {
 		this.blockID = blockID;
+	}
+	
+	public String getPreviousBlockHash() {
+		return previousBlockHash;
+	}
+	
+	public void setPreviousBlockHash(String previousBlockHash) {
+		this.previousBlockHash = previousBlockHash;
 	}
 	
 	public int getNonce() {
@@ -33,35 +35,37 @@ public class Block {
 		this.nonce = nonce;
 	}
 	
-	public String getData() {
-		return data;
-	}
-	
-	public void setData(String data) {
-		this.data = data;
-	}
-	
-	public Block(int blockID, String previousBlockHash, int nonce, String data) {
+	public Block(int blockID, String previousBlockHash, int nonce, ArrayList<Transaction> transactionList) {
 		this.blockID = blockID;
 		this.previousBlockHash = previousBlockHash;
 		this.nonce = nonce;
-		this.data = data;
+		this.transactionList = transactionList;
 	}
 	
-	//현재 특정한 블록이 가지는 정보 출력
-	public void getInformation() {
+	public void addTransaction(Transaction transaction) {
+		 transactionList.add(transaction);
+	}
+	
+	public void showInformation() {
 		System.out.println("--------------------------------------");
 		System.out.println("블록 번호: " + getBlockID());
 		System.out.println("이전 해시: " + getPreviousBlockHash());
 		System.out.println("채굴 변수 값: " + getNonce());
-		System.out.println("블록 데이터: " + getData());
+		System.out.println("트랜잭션 개수: " + transactionList.size() + "개");
+		for(int i = 0; i < transactionList.size(); i++) {
+			System.out.println(transactionList.get(i).getInformation());
+		}
+		
 		System.out.println("블록 해시: " + getBlockHash());
 		System.out.println("--------------------------------------");
 	}
 	
-	
 	public String getBlockHash() {
-		return Util.getHash(nonce + data + previousBlockHash);
+		String transactionInformations = "";
+		for(int i = 0; i < transactionList.size(); i++) {
+			transactionInformations += transactionList.get(i).getInformation();
+		}
+		return Util.getHash(nonce + transactionInformations + previousBlockHash);
 	}
 	
 	public void mine() {
@@ -69,8 +73,8 @@ public class Block {
 			if(getBlockHash().substring(0, 4).equals("0000")) {
 				System.out.println(blockID + "번째 블록의 채굴에 성공했습니다.");
 				break;
-		    }
-		    nonce++;
+			}
+			nonce++;
 		}
 	}
 
