f41be878298036ca9b13b3f56c425756d10e928e
hayoung <wizardlover_@naver.com>
2018-06-23 13:31:39 +0900

    feat: Create Project

---
+++ Spring Security/remember-me-with-redis/src/main/java/com/secutest/RememberMeWithRedisApplication.java
@@ -0,0 +1,12 @@
+package com.secutest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RememberMeWithRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RememberMeWithRedisApplication.class, args);
+	}
+}
---
+++ Spring Security/remember-me-with-redis/src/main/java/com/secutest/security/WebSecurity.java
@@ -0,0 +1,1 @@
+package com.secutest.security;
