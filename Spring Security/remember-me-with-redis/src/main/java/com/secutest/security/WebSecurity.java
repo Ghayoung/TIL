5c5ee3d513dcfbc51d56afae3c9b4fd1eee536f7
hayoung <wizardlover_@naver.com>
2018-06-23 13:55:36 +0900

    feat: Add Authentication Mechanism

--- Spring Security/remember-me-with-redis/src/main/java/com/secutest/security/WebSecurity.java
+++ Spring Security/remember-me-with-redis/src/main/java/com/secutest/security/WebSecurity.java
@@ -1,11 +1,28 @@
 package com.secutest.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic()
+		.and()
+		.authorizeRequests()
+			.anyRequest().authenticated()
+		.and()
+		.formLogin()
+			.loginPage("/login")
+			.usernameParameter("name")
+			.passwordParameter("password")
+			.defaultSuccessUrl("/")
+			.failureUrl("/login?error")
+			.permitAll();
+	}
 
 }
