70312dfe3c13d41e35638190841d408b0e4866de
hayoung <wizardlover_@naver.com>
2018-06-23 15:31:05 +0900

    feat: Add getter/setter

--- Spring Security/spring-security-basic/src/main/java/com/secutest/domain/User.java
+++ Spring Security/spring-security-basic/src/main/java/com/secutest/domain/User.java
@@ -19,4 +19,48 @@ public class User {
     String email;
     boolean enabled;
     String userType;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getLoginId() {
+		return loginId;
+	}
+	public void setLoginId(String loginId) {
+		this.loginId = loginId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public boolean isEnabled() {
+		return enabled;
+	}
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	public String getUserType() {
+		return userType;
+	}
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
 }
