ffe1a9e614e6f0fc38a10d680f7c7565d0fe6e45
hayoung <wizardlover_@naver.com>
2018-06-25 23:42:12 +0900

    feat: Add reading keys

--- Blockchain/PEM_keys/src/core/BlockChainStarter.java
+++ Blockchain/PEM_keys/src/core/BlockChainStarter.java
@@ -15,16 +15,20 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import util.EC;
 
 public class BlockChainStarter {
-	
-  public static void main(String[] args) throws Exception {
-	  
-	  //바운시 캐슬의 암호화 라이브러리를 사용하도록 설정합니다.
-	  Security.addProvider(new BouncyCastleProvider());
-	  
-	  //타원 곡선 객체를 생성해 개인키와 공개키를 각각 private.pem, public.pem으로 저장합니다.
-	  EC ec = new EC();
-	  ec.generate("private.pem", "public.pem");
-	  
-  }
+
+	public static void main(String[] args) throws Exception {
+
+		//바운시 캐슬의 암호화 라이브러리를 사용하도록 설정합니다.
+		Security.addProvider(new BouncyCastleProvider());
+
+		//타원 곡선 객체를 생성해 개인키와 공개키를 각각 private.pem, public.pem으로 저장합니다.
+		EC ec = new EC();
+		ec.generate("private.pem", "public.pem");
+
+		//파일로 저장한 개인키와 공개키를 다시 프로그램으로 불러옵니다.
+		PrivateKey privateKey = ec.readPrivateKeyFromPemFile("private.pem");
+		PublicKey publicKey = ec.readPublicKeyFromPemFile("public.pem");
+
+	}
 
 }
