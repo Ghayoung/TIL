e72b40f0948b355f00e3d4d7a29aba9031d3597e
hayoung <wizardlover_@naver.com>
2018-06-26 15:19:44 +0900

    feat: Add method to UserRepository

--- Spring Security/remember-me-with-redis/src/main/java/com/secutest/repository/UserRepository.java
+++ Spring Security/remember-me-with-redis/src/main/java/com/secutest/repository/UserRepository.java
@@ -2,7 +2,11 @@ package com.secutest.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.secutest.entity.User;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
+	User findFirstByUsername(String username);
+	User findOne(int id);
 
 }
