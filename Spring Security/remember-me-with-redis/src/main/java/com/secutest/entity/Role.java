d8482137e0c9235fc4eabd907c02fbbcdbd887f4
hayoung <wizardlover_@naver.com>
2018-06-26 15:16:50 +0900

    feat: Add columns

--- Spring Security/remember-me-with-redis/src/main/java/com/secutest/entity/Role.java
+++ Spring Security/remember-me-with-redis/src/main/java/com/secutest/entity/Role.java
@@ -1,6 +1,15 @@
 package com.secutest.entity;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -9,5 +18,19 @@ import lombok.Data;
 @Entity
 @Table(name = "ROLE")
 public class Role {
+
+    @Id
+    @Column(name = "roleId")
+    private Long id;
+
+    @Column(name = "role")
+    private String role;
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    private User user;
+
+    @OneToMany(mappedBy = "role", fetch = FetchType.EAGER ,cascade = CascadeType.PERSIST)
+    private List<RolePermission> permissions;
 
 }
