c316268f6dd9dc35109fa2cbf044f954cf09b889
hayoung <wizardlover_@naver.com>
2018-06-26 14:35:23 +0900

    feat: Add get role & permission

--- Spring Security/remember-me-with-redis/src/main/java/com/secutest/service/UserDetailsServiceImpl.java
+++ Spring Security/remember-me-with-redis/src/main/java/com/secutest/service/UserDetailsServiceImpl.java
@@ -1,6 +1,10 @@
 package com.secutest.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -22,7 +26,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		//DB에서 username으로 검색해서 user를 찾는다.
+		//DB에서 username으로 검색해서 user를 찾는다
         User user = userRepository.findFirstByUsername(username);
 
         //user를 찾지 못하면 예외를 발생시킨다
@@ -31,6 +35,22 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         }
 
         String password = user.getPassword();
+
+        //user의 role을 가져온다
+        List<Role> roles = user.getRoles();
+
+        //role이 없으면 예외를 발생시킨다
+        if (roles.isEmpty()) {
+            throw new BadCredentialsException("Authentication Failed. User granted authority is empty.");
+        }
+
+        //role 아이디를 리스트로 만든다
+        List<Long> roleIds = roles.stream()
+                                  .map(Role::getId)
+                                  .collect(Collectors.toList());
+
+        //roleIds를 통해 permission 리스트를 만든다
+        List<String> permissions = rolePermissionRepository.permissions(roleIds);
 	}
 
 }
