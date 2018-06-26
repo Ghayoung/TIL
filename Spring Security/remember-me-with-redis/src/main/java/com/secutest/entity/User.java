8d3b9641a8e338a38369c8ab95bd9bf18be3d9e4
hayoung <wizardlover_@naver.com>
2018-06-26 15:17:31 +0900

    feat: Add columns to User.java

--- Spring Security/remember-me-with-redis/src/main/java/com/secutest/entity/User.java
+++ Spring Security/remember-me-with-redis/src/main/java/com/secutest/entity/User.java
@@ -1,6 +1,15 @@
 package com.secutest.entity;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -9,5 +18,28 @@ import lombok.Data;
 @Entity
 @Table(name="USER")
 public class User {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER ,cascade = CascadeType.PERSIST)
+    private List<Role> roles;
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '}';
+    }
 
 }
