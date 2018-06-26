068894c1ea06939e975a967ca51cf1d641ea40da
hayoung <wizardlover_@naver.com>
2018-06-26 15:15:36 +0900

    feat: Create RolePermission.java

---
+++ Spring Security/remember-me-with-redis/src/main/java/com/secutest/entity/RolePermission.java
@@ -0,0 +1,13 @@
+package com.secutest.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "ROLE_PERMISSION")
+public class RolePermission {
+
+}
