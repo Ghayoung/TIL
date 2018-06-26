b20d483b790d7b99d6788e9002a9668015343e8d
hayoung <wizardlover_@naver.com>
2018-06-26 14:48:29 +0900

    feat: Add constructor

--- Spring Security/remember-me-with-redis/src/main/java/com/secutest/util/JsonUtils.java
+++ Spring Security/remember-me-with-redis/src/main/java/com/secutest/util/JsonUtils.java
@@ -1,5 +1,30 @@
 package com.secutest.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
 public class JsonUtils {
+
+	private static final Logger logger = LoggerFactory.getLogger(JsonUtils.class);
+
+	private final ObjectMapper objectMapper;
+
+	private JsonUtils() {
+		objectMapper = new ObjectMapper();
+		objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+		objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		objectMapper.configure(MapperFeature.AUTO_DETECT_GETTERS, true);
+		objectMapper.configure(MapperFeature.AUTO_DETECT_IS_GETTERS, true);
+		objectMapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
+		objectMapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, false);
+	}
 
 }
