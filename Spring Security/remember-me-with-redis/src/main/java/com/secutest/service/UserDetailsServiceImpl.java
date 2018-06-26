6e35a72d95cad176045c31d6f0901a1fd608aa03
hayoung <wizardlover_@naver.com>
2018-06-26 14:27:18 +0900

    feat: Import class

--- Spring Security/remember-me-with-redis/src/main/java/com/secutest/service/UserDetailsServiceImpl.java
+++ Spring Security/remember-me-with-redis/src/main/java/com/secutest/service/UserDetailsServiceImpl.java
@@ -1,3 +1,11 @@
 package com.secutest.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
 //UserDetailsService를 구현하는 클래스
+
+@Component
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+}
