9e39ab612426d582af90ac180dab5ff5251a53fc
hayoung <wizardlover_@naver.com>
2018-06-26 15:13:41 +0900

    feat: Create Role.java

---
+++ Spring Security/remember-me-with-redis/src/main/java/com/secutest/entity/Role.java
@@ -0,0 +1,13 @@
+package com.secutest.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "ROLE")
+public class Role {
+
+}
