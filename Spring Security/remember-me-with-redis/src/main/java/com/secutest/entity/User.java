9323b4553ec2debe5528f1eaeab85a61ac8451d6
hayoung <wizardlover_@naver.com>
2018-06-26 15:11:56 +0900

    feat: Create User.java

---
+++ Spring Security/remember-me-with-redis/src/main/java/com/secutest/entity/User.java
@@ -0,0 +1,13 @@
+package com.secutest.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="USER")
+public class User {
+
+}
