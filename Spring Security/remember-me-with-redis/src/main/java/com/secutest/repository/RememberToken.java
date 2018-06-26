08c5840e3141a1c8aff380707e8763ece00b264e
hayoung <wizardlover_@naver.com>
2018-06-26 14:43:36 +0900

    feat: Add attributes & getter & setter

--- Spring Security/remember-me-with-redis/src/main/java/com/secutest/repository/RememberToken.java
+++ Spring Security/remember-me-with-redis/src/main/java/com/secutest/repository/RememberToken.java
@@ -1,10 +1,48 @@
 package com.secutest.repository;
 
+import java.util.Date;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class RememberToken {
+
+	private final String username; //유저명
+
+	private String series; //코드
+
+	private String tokenValue; //토큰값
+
+	private Date date; //토큰생성일
+
+	public String getSeries() {
+		return series;
+	}
+
+	public void setSeries(String series) {
+		this.series = series;
+	}
+
+	public String getTokenValue() {
+		return tokenValue;
+	}
+
+	public void setTokenValue(String tokenValue) {
+		this.tokenValue = tokenValue;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getUsername() {
+		return username;
+	}
 
 }
