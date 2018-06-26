e1a57b0a84df1ecb4b971c954404231e9b9cbc1b
hayoung <wizardlover_@naver.com>
2018-06-26 14:29:14 +0900

    feat: Add attributes

--- Spring Security/remember-me-with-redis/src/main/java/com/secutest/service/UserDetailsServiceImpl.java
+++ Spring Security/remember-me-with-redis/src/main/java/com/secutest/service/UserDetailsServiceImpl.java
@@ -1,11 +1,28 @@
 package com.secutest.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
+
+import com.secutest.repository.RolePermissionRepository;
+import com.secutest.repository.UserRepository;
 
 //UserDetailsService를 구현하는 클래스
 
 @Component
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository; //생성해야함
+
+    @Autowired
+    private RolePermissionRepository rolePermissionRepository; //생성해야함
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+	}
 
 }
