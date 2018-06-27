47976400487cd4153b084d2a6b4a010168c22d2b
hayoung <wizardlover_@naver.com>
2018-06-27 21:12:39 +0900

    feat: Add attributes

---
+++ Spring Security/remember-me-with-redis/src/main/java/com/secutest/data/SecurityUrlData.java
@@ -0,0 +1,25 @@
+package com.secutest.data;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix="security.url")
+public class SecurityUrlData {
+
+	private String login;
+
+	private String logout;
+
+	private String mypage;
+
+    private String accessdenied;
+
+    private String refreshToken;
+
+
+
+}
