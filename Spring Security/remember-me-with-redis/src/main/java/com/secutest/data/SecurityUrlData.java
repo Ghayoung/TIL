5d9941c7c35813341733063e2af21fd69471b75c
hayoung <wizardlover_@naver.com>
2018-06-28 19:31:29 +0900

    feat: Add getter/setter

--- Spring Security/remember-me-with-redis/src/main/java/com/secutest/data/SecurityUrlData.java
+++ Spring Security/remember-me-with-redis/src/main/java/com/secutest/data/SecurityUrlData.java
@@ -20,6 +20,44 @@ public class SecurityUrlData {
 
     private String refreshToken;
 
+	public String getLogin() {
+		return login;
+	}
 
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getLogout() {
+		return logout;
+	}
+
+	public void setLogout(String logout) {
+		this.logout = logout;
+	}
+
+	public String getMypage() {
+		return mypage;
+	}
+
+	public void setMypage(String mypage) {
+		this.mypage = mypage;
+	}
+
+	public String getAccessdenied() {
+		return accessdenied;
+	}
+
+	public void setAccessdenied(String accessdenied) {
+		this.accessdenied = accessdenied;
+	}
+
+	public String getRefreshToken() {
+		return refreshToken;
+	}
+
+	public void setRefreshToken(String refreshToken) {
+		this.refreshToken = refreshToken;
+	}
 
 }
