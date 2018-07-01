d3212f6ac171bee984867c157e006f21f80979c5
hayoung <wizardlover_@naver.com>
2018-07-01 22:25:15 +0900

    feat: Add login()

--- Spring Security/remember-me-with-redis/src/main/java/com/secutest/controller/MainController.java
+++ Spring Security/remember-me-with-redis/src/main/java/com/secutest/controller/MainController.java
@@ -1,8 +1,15 @@
 package com.secutest.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
+
+	@RequestMapping(value = {"/", "/login"}, method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
 
 }
